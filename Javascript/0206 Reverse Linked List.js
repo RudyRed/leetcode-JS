ddcd60f7aea127aa5fce819e54098b34e4e0aaf4
RudyRed <willgross94@gmail.com>
2018-01-18 11:49:44 -0800

    Completed

---
+++ Javascript/0206 Reverse Linked List.js
@@ -0,0 +1,43 @@
+// Reverse a singly linked list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function(head) {
+  if (!head) return head;
+  let curr = head;
+  let prev = null;
+  let placeholder;
+
+  while(placeholder !== null) {
+    placeholder = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = placeholder;
+  }
+
+  return prev;
+};
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+// let l = [2, 3, 4, 5];
+let list = new ListNode(1);
+// let currentNode = list;
+// for (let num of l) {
+//   currentNode.next = new ListNode(num);
+//   currentNode = currentNode.next;
+// }
+
+console.log(reverseList(list));
