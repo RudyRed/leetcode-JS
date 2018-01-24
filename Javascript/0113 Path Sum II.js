8ec72bc9f705aca598115696d449bcd6489f7024
RudyRed <willgross94@gmail.com>
2018-01-23 19:21:24 -0500

    Completed

---
+++ Javascript/0113 Path Sum II.js
@@ -0,0 +1,85 @@
+// Given a binary tree and a sum, find all root-to-leaf paths where each path's
+// sum equals the given sum.
+//
+// For example:
+// Given the below binary tree and sum = 22,
+//               5
+//              / \
+//             4   8
+//            /   / \
+//           11  13  4
+//          /  \    / \
+//         7    2  5   1
+// return
+// [
+//    [5,4,11,2],
+//    [5,8,4,5]
+// ]
+
+const pathSum = function(root, sum, currentSet = [[]]) {
+  if (!root) return [];
+
+  if (!root.left && !root.right) {
+    if (sum - root.val) return [];
+    return currentSet.map(x => x.concat(root.val));
+  }
+
+  let output = [];
+  if (root.right) {
+    let right = pathSum(root.right, sum - root.val, currentSet.map(x => x.concat(root.val)));
+    if (right.length) {
+      for (let set of right) output.push(set);
+    }
+  }
+
+  if (root.left) {
+    let left = pathSum(root.left, sum - root.val, currentSet.map(x => x.concat(root.val)));
+    if (left.length) {
+      for (let set of left) output.push(set);
+    }
+  }
+  return output
+};
+
+// let tree = {
+//   val: 5,
+//   left: {
+//     val: 4,
+//     left: {
+//       val: 11,
+//       left: {
+//         val: 7,
+// 				left: null,
+//         right: null
+//       },
+//       right: {
+//         val: 2,
+//         left: null,
+//         right: null
+//       }
+//     },
+//     right: null
+//   },
+//   right: {
+//     val: 8,
+//     left: {
+//       val: 13,
+//       left: null,
+//       right: null
+//     },
+//     right: {
+//       val: 4,
+//       left: {
+//       	val: 5,
+//       	left: null,
+//       	right: null
+//       },
+//       right: {
+//         val: 1,
+//         left: null,
+//         right: null
+//       }
+//     }
+//   }
+// }
+// console.log(pathSum(tree, 22));
