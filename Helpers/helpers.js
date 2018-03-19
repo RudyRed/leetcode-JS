3f034493a1757296eb47ba442c27e114fdb0ce30
RudyRed <willgross94@gmail.com>
2018-03-19 16:25:51 -0400

    Fixed error with null

--- Helpers/helpers.js
+++ Helpers/helpers.js
@@ -7,15 +7,30 @@ const makeBT = function (arr) {
   const tree = new TreeNode(arr[0]);
   let pending = [];
   let parent = tree;
+  let leftFlag = true;
 
   for (let i = 1; i < arr.length; i++) {
-    if (parent.left === null) {
-      parent.left = new TreeNode(arr[i]);
-      pending.push(parent.left);
+    if (leftFlag) {
+
+      if (arr[i] === null) {
+        parent.left = null;
+      } else {
+        parent.left = new TreeNode(arr[i]);
+        pending.push(parent.left);
+      }
+      leftFlag = false;
+
     } else {
-      parent.right = new TreeNode(arr[i]);
-      pending.push(parent.right);
+
+      if (arr[i] === null) {
+        parent.right = null;
+      } else {
+        parent.right = new TreeNode(arr[i]);
+        pending.push(parent.right);
+      }
       parent = pending.shift();
+      leftFlag = true;
+
     }
   }
 
