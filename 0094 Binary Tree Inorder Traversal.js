22dcc4da4a8bac354a9bb2d98df801b0ecf8fc32
RudyRed <willgross94@gmail.com>
2018-03-20 15:57:36 -0400

    0094 Cleaner

--- 0094 Binary Tree Inorder Traversal.js
+++ 0094 Binary Tree Inorder Traversal.js
@@ -10,43 +10,58 @@
 // return [1,3,2].
 const { makeBT } = require('./Helpers/helpers.js');
 
-// const TreeNode = function (val) {
-//   this.val = val;
-//   this.right = this.left = null;
-// };
 
 const inorderTraversal = function (root) {
-  if (!root) return [];
-  const pendingNodes = [root];
+  const pendingNodes = [];
   const output = [];
 
-  while (pendingNodes.length) {
-    const lastPending = pendingNodes[pendingNodes.length - 1];
-
-    if (lastPending.left) {
-      pendingNodes.push(lastPending.left);
-      continue;
+  while (true) {
+    while (root) {
+      pendingNodes.push(root);
+      root = root.left;
     }
 
-    const removed = pendingNodes.pop();
-    output.push(removed.val);
-
-    if (pendingNodes.length) {
-      const previous = pendingNodes[pendingNodes.length - 1];
-
-      if (previous.left) {
-        previous.left = null;
-      }
-
-    }
-
-    if (removed.right) pendingNodes.push(removed.right);
-
+    if (pendingNodes.length == 0) return output;
+    root = pendingNodes.pop();
+    output.push(root.val);
+    root = root.right;
   }
 
-  return output;
 };
 
 console.log(inorderTraversal(makeBT([1,null,2,3])), 'sb [1,3,2]');
 console.log(inorderTraversal(makeBT([1,2,3,4,5,6,7])), 'sb [4,2,5,1,6,3,7]');
 console.log(inorderTraversal(makeBT([1,null,2,null,3])), 'sb [1,2,3]');
+
+// slightly less cleary alternative
+// const inorderTraversal = function (root) {
+//   if (!root) return [];
+//   const pendingNodes = [root];
+//   const output = [];
+//
+//   while (pendingNodes.length) {
+//     const lastPending = pendingNodes[pendingNodes.length - 1];
+//
+//     if (lastPending.left) {
+//       pendingNodes.push(lastPending.left);
+//       continue;
+//     }
+//
+//     const removed = pendingNodes.pop();
+//     output.push(removed.val);
+//
+//     if (pendingNodes.length) {
+//       const previous = pendingNodes[pendingNodes.length - 1];
+//
+//       if (previous.left) {
+//         previous.left = null;
+//       }
+//
+//     }
+//
+//     if (removed.right) pendingNodes.push(removed.right);
+//
+//   }
+//
+//   return output;
+// };
