b2b8a841dbb71486a1a5318d21522887fee6776a
RudyRed <willgross94@gmail.com>
2018-03-20 15:06:01 -0400

    0094 Completed

---
+++ 0094 Binary Tree Inorder Traversal.js
@@ -0,0 +1,52 @@
+// Given a binary tree, return the inorder traversal of its nodes' values.
+//
+// For example:
+// Given binary tree [1,null,2,3],
+//    1
+//     \
+//      2
+//     /
+//    3
+// return [1,3,2].
+const { makeBT } = require('./Helpers/helpers.js');
+
+// const TreeNode = function (val) {
+//   this.val = val;
+//   this.right = this.left = null;
+// };
+
+const inorderTraversal = function (root) {
+  if (!root) return [];
+  const pendingNodes = [root];
+  const output = [];
+
+  while (pendingNodes.length) {
+    const lastPending = pendingNodes[pendingNodes.length - 1];
+
+    if (lastPending.left) {
+      pendingNodes.push(lastPending.left);
+      continue;
+    }
+
+    const removed = pendingNodes.pop();
+    output.push(removed.val);
+
+    if (pendingNodes.length) {
+      const previous = pendingNodes[pendingNodes.length - 1];
+
+      if (previous.left) {
+        previous.left = null;
+      }
+
+    }
+
+    if (removed.right) pendingNodes.push(removed.right);
+
+  }
+
+  return output;
+};
+
+console.log(inorderTraversal(makeBT([1,null,2,3])), 'sb [1,3,2]');
+console.log(inorderTraversal(makeBT([1,2,3,4,5,6,7])), 'sb [4,2,5,1,6,3,7]');
+console.log(inorderTraversal(makeBT([1,null,2,null,3])), 'sb [1,2,3]');
