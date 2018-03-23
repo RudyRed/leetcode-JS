23953b942a544a44debdcce399c87f576fcdbb27
RudyRed <willgross94@gmail.com>
2018-03-23 17:00:17 -0400

    0198 Init

---
+++ 0198 House Robber.js
@@ -0,0 +1,16 @@
+// You are a professional robber planning to rob houses along a street.
+// Each house has a certain amount of money stashed, the only constraint stopping
+// you from robbing each of them is that adjacent houses have security system
+// connected and it will automatically contact the police if two adjacent houses
+// were broken into on the same night.
+//
+// Given a list of non-negative integers representing the amount of money of each
+// house, determine the maximum amount of money you can rob tonight without
+// alerting the police.
+
+const rob = function (nums) {
+
+};
+
+console.log(rob([1,2,3,4,5,6,7,8]), 'sb 20');
+console.log(rob([1,2,3,4,5,6,7,8,9]), 'sb 25');
