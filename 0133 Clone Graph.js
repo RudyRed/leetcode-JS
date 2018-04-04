fb16d3bbe0e3baae384ed1cfd2dafba16a63e7ff
RudyRed <willgross94@gmail.com>
2018-04-04 14:52:38 -0400

    0133 Completed

---
+++ 0133 Clone Graph.js
@@ -0,0 +1,68 @@
+// Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+//
+//
+// OJ's undirected graph serialization:
+// Nodes are labeled uniquely.
+//
+// We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+// As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+//
+// The graph has a total of three nodes, and therefore contains three parts as separated by #.
+//
+// First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+// Second node is labeled as 1. Connect node 1 to node 2.
+// Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+// Visually, the graph looks like the following:
+//
+//        1
+//       / \
+//      /   \
+//     0 --- 2
+//          / \
+//          \_/
+
+const UndirectedGraphNode = function (label) {
+  this.label = label;
+  this.neighbors = [];   // Array of UndirectedGraphNode
+};
+
+const cloneGraph = function(graph) {
+  if (!graph) return null;
+  let originalStack = [graph];
+  let copyStack = [new UndirectedGraphNode(graph.label)];
+  let nodeObj = {[graph.label]: copyStack[0]};
+
+
+
+  while (originalStack.length) {
+    const newCurr = copyStack.pop();
+    const originalCurr = originalStack.pop();
+
+    for (let neighbor of originalCurr.neighbors) {
+      if (nodeObj[neighbor.label]) {
+        newCurr.neighbors.push(nodeObj[neighbor.label]);
+      } else {
+
+        const newNode = new UndirectedGraphNode(neighbor.label);
+        newCurr.neighbors.push(newNode);
+        nodeObj[neighbor.label] = newNode;
+        originalStack.push(neighbor);
+        copyStack.push(newNode);
+
+      }
+    }
+
+  }
+
+  return nodeObj[graph.label];
+};
+
+let zero = new UndirectedGraphNode(0);
+let one = new UndirectedGraphNode(1);
+let two = new UndirectedGraphNode(2);
+
+zero.neighbors.push(one, two);
+one.neighbors.push(zero, two);
+two.neighbors.push(two);
+
+console.log(cloneGraph(zero));
