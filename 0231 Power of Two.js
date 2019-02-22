2e1ef468d256e7b4a38c9d15157d9447166e6853
RudyRed <will.gross@nearform.com>
2019-02-22 16:10:41 +0000

    0231 Completed

---
+++ 0231 Power of Two.js
@@ -0,0 +1,27 @@
+// Given an integer, write a function to determine if it is a power of two.
+
+// Example 1:
+
+// Input: 1
+// Output: true
+// Explanation: 20 = 1
+// Example 2:
+
+// Input: 16
+// Output: true
+// Explanation: 24 = 16
+// Example 3:
+
+// Input: 218
+// Output: false
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfTwo = n => {
+  if (!n) return false
+  const log = Math.log2(n);
+
+  return log === Math.round(log);
+};
